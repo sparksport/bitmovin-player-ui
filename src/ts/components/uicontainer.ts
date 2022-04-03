a707cbdc2ad10c97952e6459e4c0686eebb844e9
Franck P <franckpoingt@gmail.com>
2022-04-04 10:27:25 +1200

    change iOS check for airplay condition

--- src/ts/components/uicontainer.ts
+++ src/ts/components/uicontainer.ts
@@ -93,7 +93,7 @@ export class UIContainer extends Container<UIContainerConfig> {
     let isFirstTouch = true;
     let playerState: PlayerUtils.PlayerState;
     let isUiBlocked = false;
-    let isAirplayActive = BrowserUtils.isIOS && player.isAirplayActive();
+    let isAirplayInactive = BrowserUtils.isIOS && !player.isAirplayActive();
 
     const hidingPrevented = (): boolean => {
       return config.hidePlayerStateExceptions && config.hidePlayerStateExceptions.indexOf(playerState) > -1;
@@ -115,7 +115,7 @@ export class UIContainer extends Container<UIContainerConfig> {
 
     let hideUi = () => {
       // Hide the UI only if it is shown, and if not casting, and if not Airplaying on Ios
-      if (isUiShown && !player.isCasting() && !isAirplayActive) {
+      if (isUiShown && !player.isCasting() && isAirplayInactive) {
         // Issue a preview event to check if we are good to hide the controls
         let previewHideEventArgs = <CancelEventArgs>{};
         uimanager.onPreviewControlsHide.dispatch(this, previewHideEventArgs);
